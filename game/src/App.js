421e8809a66fffe6714ac7dd55acf7916c42ced1
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-07-15 21:26:01 -0400

    Adds basic game board

--- game/src/App.js
+++ game/src/App.js
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useEffect } from "react";
+
+import Cell from "./Cell.js";
+import { initialState, reducer } from "./reducer/";
+
+import styles from "./styles.module.scss";
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    const calcNextGrid = () => {
+      const nextGrid = state.grid.map((row, y) =>
+        row.map((cell, x) => {
+          const rows = state.grid.length;
+          const cols = row.length;
+          const left = (cols + x - 1) % cols;
+          const right = (cols + x + 1) % cols;
+          const up = (rows + y - 1) % rows;
+          const down = (rows + y + 1) % rows;
+
+          const localPopulation = [
+            [left, up],
+            [x, up],
+            [right, up],
+            [left, y],
+            [right, y],
+            [left, down],
+            [x, down],
+            [right, down]
+          ].reduce((acc, [x, y]) => {
+            const add = state.grid[y][x] ? 1 : 0;
+            return acc + add;
+          }, 0);
+
+          // console.log(
+          //   `Currently at: [${x}, ${y}]  |  Value: ${
+          //     state.grid[y][x]
+          //   }  |  Local Pop: ${localPopulation}`
+          //   // }  |  Direction: ${up} ${right} ${down} ${left}`
+          // );
+          return (
+            (cell && localPopulation === 2) ||
+            localPopulation === 3 ||
+            (!cell && localPopulation === 3)
+          );
+        })
+      );
+      dispatch({ type: "UPDATE_GRID", payload: nextGrid });
+    };
+
+    const interval = setInterval(calcNextGrid, state.time);
+    return () => clearInterval(interval);
+  }, [state.grid, state.time]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {state.grid.map((row, y) => (
+        <div key={y} className={styles.row}>
+          {row.map((_, x) => (
+            <Cell
+              key={`${x}, ${y}`}
+              isAlive={state.grid[y][x]}
+              position={{ x, y }}
+            />
+          ))}
+        </div>
+      ))}
+      {/* <button onClick={() => calcNextGrid()}>Next</button> */}
     </div>
   );
 }
